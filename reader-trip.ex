6f7e2263ddccd4e4f142bbf0469d074342b274db
enrique pastor cisneros <kikepastcis@gmail.com>
2023-04-27 10:26:16 +0200

    wip: Process file and extract to a map list

---
+++ reader-trip.ex
@@ -0,0 +1,30 @@
+defmodule Reader do
+
+  def format_item(line) do
+    IO.puts "The processed line is: #{line}"
+
+    Regex.named_captures(~r/\s(?<transport>[A-Za-z]+)\s(?<origin>[A-Za-z]+)\s(?<dateDestiny>\d{4}-\d{2}-\d{2})\s?(?<hourOrigin>(\d{2}:\d{2})?)\s->\s?(?<dateOrigin>(\d{4}-\d{2}-\d{2})?)\s?(?<destiny>([A-Za-z]+)?)\s?(?<hourDestiny>(\d{2}:\d{2})?).*/, line)
+  end
+  def read_file() do
+    file = "trip.txt"
+
+    if File.exists?(file) do
+      {:ok, content} = File.read(file)
+
+      formatContent = content
+        |> String.replace("\r\n", "")
+        |> String.replace("RESERVATION", "")
+        |> String.split("SEGMENT:", trim: true)
+        |> tl()
+        |> Enum.map(fn line -> format_item(line) end)
+
+
+        IO.inspect "formatContent: #{formatContent}"
+
+    else
+      IO.puts "El archivo no se encuentra"
+    end
+  end
+end
+
+Reader.read_file()
