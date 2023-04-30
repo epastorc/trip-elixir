db3ad26acdf437acde4b4ebbf7b9abdd6d87d0ef
Enrique Pastor Cisneros <kikepastcis@gmail.com>
2023-04-30 13:22:25 +0200

    feat: added get base on functionality

--- lib/trip.ex
+++ lib/trip.ex
@@ -1,15 +1,22 @@
 defmodule Reader do
 
-  def format_item(line) do
-    IO.puts "The processed line is: #{line}"
+  def get_base_on(file_content) do
+  match = Regex.named_captures(~r/.*BASED:\s*(?<based>[A-Z]{3})/, file_content)
 
+  match["based"]
+  end
+
+  def format_item(line) do
     Regex.named_captures(~r/\s(?<transport>[A-Za-z]+)\s(?<origin>[A-Za-z]+)\s(?<dateDestiny>\d{4}-\d{2}-\d{2})\s?(?<hourOrigin>(\d{2}:\d{2})?)\s->\s?(?<dateOrigin>(\d{4}-\d{2}-\d{2})?)\s?(?<destiny>([A-Za-z]+)?)\s?(?<hourDestiny>(\d{2}:\d{2})?).*/, line)
   end
+
   def read_file() do
     file = "input.txt"
 
     if File.exists?(file) do
       {:ok, content} = File.read(file)
+
+      based_on = get_base_on(content)
 
       formatContent = content
         |> String.replace("\r\n", "")
